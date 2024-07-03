d28d94ef038399791fd11426c25e334f505d7a89
milindrai <milindrai365@gmail.com>
2024-07-03 19:36:23 +0530

    Update Game class to use chess.js library for game logic

--- backend1/src/Game.ts
+++ backend1/src/Game.ts
@@ -1,21 +1,75 @@
 import { WebSocket } from "ws";
+import { Chess } from 'chess.js'
+import { INIT_GAME, MOVE } from "./messages";
 
 export class Game{
     public player1:WebSocket;
     public player2:WebSocket;
-    private board:string;
-    private moves:string[];
+    public board:Chess;
     private startTime:Date;
 
     constructor(player1:WebSocket,player2:WebSocket){
         this.player1=player1;
         this.player2=player2;
-        this.board="";
-        this.moves=[];
+        this.board=new Chess();
         this.startTime=new Date();
+        this.player1.send(JSON.stringify({
+            type:INIT_GAME,
+            payload:{
+                color:"white"
+            }
+        }));
+        this.player2.send(JSON.stringify({
+            type:INIT_GAME,
+            payload:{
+                color:"black"
+            }
+        }));
     }
 
-    makeMove(socket:WebSocket,move:string){
-        
+    makeMove(socket:WebSocket,move:{
+        from:string,
+        to:string,
+    }){
+        if(socket !== this.player1 && this.board.moves.length%2 === 0){
+            return;
+        }
+        if(socket !== this.player2 && this.board.moves.length%2 === 1){
+            return;
+        }
+        try {
+            this.board.move(move); 
+        } catch (error) {
+            return;
+        }
+
+        if(this.board.isGameOver()){
+            this.player1.send(JSON.stringify({
+                type:'GAME_OVER',
+                //winner:this.board.turn()==='w'?'player1':'player2',
+                winner:this.board.turn()==='w'?'black':'white',
+                time:new Date().getTime()-this.startTime.getTime()
+            }));
+            this.player2.send(JSON.stringify({
+                type:'GAME_OVER',
+                //winner:this.board.turn()==='w'?'player2':'player1',
+                winner:this.board.turn()==='w'?'black':'white',
+                time:new Date().getTime()-this.startTime.getTime()
+            })); 
+        }
+
+        if(this.board.moves.length%2 === 0){
+            this.player2.send(JSON.stringify({
+                type:MOVE,
+                payload:move,
+            }));
+        }
+        else{
+            this.player1.send(JSON.stringify({
+                type:MOVE,
+                payload:move,
+            }));
+        }
+                    
     }
 }
